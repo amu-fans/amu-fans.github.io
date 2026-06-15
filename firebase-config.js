b3f67f28ac5f0e420c654206b5325eff90b05899
amu-fans <dq.kitayama2025@gmail.com>
2026-06-15 21:55:48 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// このファイルにだけ本物のAPIキー等の設定を書いておきます
+const firebaseConfig = {
+    apiKey: "AIzaSyBOaiV7Uy30KvFi38V-pKJA25oZ-D-2UUY",
+	authDomain: "amu-clicker.firebaseapp.com",
+	databaseURL: "https://amu-clicker-default-rtdb.firebaseio.com/",
+    projectId: "amu-clicker",
+    storageBucket: "amu-clicker.firebasestorage.app",
+    messagingSenderId: "413500240591",
+    appId: "1:413500240591:web:c36084b3ba4c801a5dfdba"
+};
